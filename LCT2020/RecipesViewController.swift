79a4b815f1d712e96a142484a4e03644b6131f71
Cora-Jane Meade <corajanemeade@gmail.com>
2020-05-17 18:25:29 +0100

    URL session complete

--- LCT2020/RecipesViewController.swift
+++ LCT2020/RecipesViewController.swift
@@ -57,15 +57,6 @@ class RecipesViewController: UIViewController {
         return url!
     }
     
-    func performRequest(with url: URL) -> Data? {
-        do {
-            return try Data(contentsOf: url)
-        } catch {
-            print("Download Error: \(error.localizedDescription)")
-            showNetworkError()
-            return nil
-        }
-    }
     
     func parse(data: Data) -> [RecipeSearchResult] {
         do {
@@ -96,19 +87,34 @@ extension RecipesViewController: UISearchBarDelegate {
             tableView.reloadData()
             hasSearched = true
             searchResults = []
-            let queue = DispatchQueue.global()
-            let url = self.recipePuppyURL(searchText: searchBar.text!)
-            queue.async {
-                if let data = self.performRequest(with: url) {
-                    self.searchResults = self.parse(data: data)
-                    self.searchResults.sort(by: <)
+            let url = recipePuppyURL(searchText: searchBar.text!)
+            let session = URLSession.shared
+            let dataTask = session.dataTask(with: url,
+                completionHandler: { data, response, error in
+                    if let error = error {
+                        print("Failure! \(error.localizedDescription)")
+                    } else if let httpResponse = response as? HTTPURLResponse,
+                        httpResponse.statusCode == 200 {
+                        if let data = data {
+                            self.searchResults = self.parse(data: data)
+                            self.searchResults.sort(by: <)
+                            DispatchQueue.main.async {
+                                self.isLoading = false
+                                self.tableView.reloadData()
+                            }
+                            return
+                        }
+                    } else {
+                        print("Failure! \(response!)")
+                    }
                     DispatchQueue.main.async {
+                        self.hasSearched = false
                         self.isLoading = false
                         self.tableView.reloadData()
+                        self.showNetworkError()
                     }
-                    return
-                }
-            }
+            })
+            dataTask.resume()
         }
     }
     
